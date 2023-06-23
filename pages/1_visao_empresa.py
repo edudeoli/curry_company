5614f259a09670407bf30c731a005b63f5577f7c
Edu 'Deoli' Ojr <102696976+edudeoli@users.noreply.github.com>
2023-06-23 12:25:53 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -174,8 +174,8 @@ st.sidebar.markdown( '## Selecione uma data limite' )
 date_slider = st.sidebar.slider(
     'Até qual valor?',
     value = datetime.datetime( 2022, 2, 1 ),
-    min_value = pd.datetime( 2022, 2, 11 ),
-    max_value = pd.datetime( 2022, 4, 6 ),
+    min_value = datetime.datetime( 2022, 2, 11 ),
+    max_value = datetime.datetime( 2022, 4, 6 ),
     format = 'DD-MM-YYYY' )
 
 st.sidebar.markdown( """___""" )
